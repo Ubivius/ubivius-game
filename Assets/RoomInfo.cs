c9faa170b5f6ae0cb8c94c118e112b7499ba9ad4
Jérôme Paquet <jerome.paquet@usherbrooke.ca>
2021-01-23 16:08:54 -0500

    Début de création de préfab

---
+++ Assets/RoomInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ubv
+{
+    namespace common
+    {
+        public class RoomInfo : MonoBehaviour
+        {
+            // Start is called before the first frame update
+
+            [SerializeField] private int m_height;
+            [SerializeField] private int m_width;
+            
+        }
+    }
+    
+}
+
