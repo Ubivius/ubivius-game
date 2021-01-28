6ee1f57ac38ff5ec50eca183f649cf4fce3640b6
Charles <charles.murphy.042@gmail.com>
2021-01-28 09:24:44 -0500

    gitignore asset/editor/build exception

---
+++ Assets/Editor/Build/Build.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using System;
+
+public class Build
+{
+    static void ServerBuild()
+    {
+        BuildPlayerOptions build = new BuildPlayerOptions();
+        build.scenes = new[] {
+            "ServerScene",
+            "proto_move_server"
+        };
+        build.locationPathName = "ubivius_server";
+        build.target = BuildTarget.StandaloneWindows64;
+        build.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(build);
+    }
+
+    static void ClientBuild()
+    {
+        BuildPlayerOptions build = new BuildPlayerOptions();
+        build.scenes = new[] {
+            "ClientScene",
+            "proto_move_client"
+        };
+        build.locationPathName = "ubivius_client";
+        build.target = BuildTarget.StandaloneWindows64;
+        build.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(build);
+    }
+}
