d9e24ab99cd8f30967e8ec135f5c1a5f1bfc7b67
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-02-01 16:51:00 -0500

    EnemyStateTempalte

---
+++ Assets/Scripts/EnemySystem/EnemyStatePattern/EnemyState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ubv.server.logic.ai
+{
+    abstract public class EnemyState
+    {
+        // Use this for initialization
+        public void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public EnemyState Update()
+        {
+            return this;
+        }
+    }
+}
