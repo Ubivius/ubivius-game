ece4f29e3a3e23c16cc0d5657f635292a63daca4
Charles <charles.murphy.042@gmail.com>
2021-02-01 15:22:54 -0500

    Rework of CLientSyncStates and InputController

--- Assets/Scripts/Client/ClientState.cs
+++ Assets/Scripts/Client/ClientState.cs
@@ -60,59 +60,10 @@ namespace ubv
                 }
             }
 
-            public Dictionary<int, common.data.PlayerState>.ValueCollection Players()
+            public Dictionary<int, common.data.PlayerState> Players()
             {
-                return m_playerStates.Value.Values;
+                return m_playerStates.Value;
             }
-
-#region UTILITY FUNCTIONS
-            private static List<IClientStateUpdater> m_updaters = new List<IClientStateUpdater>();
-
-            static public void RegisterUpdater(IClientStateUpdater updater)
-            {
-                m_updaters.Add(updater);
-            }
-
-            static public void SetToState(ClientState state)
-            {
-                for (int i = 0; i < m_updaters.Count; i++)
-                {
-                    m_updaters[i].UpdateFromState(state);
-                }
-            }
-
-            public void StoreCurrentStateAndStep(common.data.InputFrame input, float deltaTime, ref PhysicsScene2D physics)
-            {
-                ClientState _this = this;
-
-                for (int i = 0; i < m_updaters.Count; i++)
-                {
-                    m_updaters[i].SetStateAndStep(ref _this, input, deltaTime);
-                }
-
-                physics.Simulate(deltaTime);
-            }
-
-            /// <summary>
-            /// Checks if any updater needs to correct its internal state
-            /// </summary>
-            /// <param name="remoteState">The state to compare to</param>
-            /// <returns>Updaters needing to be corrected</returns>
-            static public List<IClientStateUpdater> UpdatersNeedingCorrection(ClientState remoteState)
-            {
-                List<IClientStateUpdater> needCorrection = new List<IClientStateUpdater>();
-
-                for (int i = 0; i < m_updaters.Count; i++)
-                {
-                    if (m_updaters[i].NeedsCorrection(remoteState))
-                    {
-                        needCorrection.Add(m_updaters[i]);
-                    }
-                }
-
-                return needCorrection;
-            }
-#endregion
         }
     }
 }
---
+++ Assets/Scripts/Client/PlayerGameObjectUpdater.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using ubv.common.data;
+using System.Collections.Generic;
+
+namespace ubv.client.logic
+{
+    /// <summary>
+    /// Instantiate players and moves them according to their player states
+    /// </summary>
+    public class PlayerGameObjectUpdater : IClientStateUpdater
+    {
+        private PlayerSettings m_playerSettings;
+
+        private Dictionary<int, Rigidbody2D> m_bodies;
+        private Rigidbody2D m_localPlayerBody;
+
+        public PlayerGameObjectUpdater(ClientSyncPlay playState, PlayerSettings playerSettings, Dictionary<int, PlayerState> playerStates, int localID)
+        {
+            m_playerSettings = playerSettings;
+            m_bodies = new Dictionary<int, Rigidbody2D>();
+            foreach(int id in playerStates.Keys)
+            {
+                m_bodies[id] = GameObject.Instantiate(playerSettings.PlayerPrefab).GetComponent<Rigidbody2D>();
+            }
+
+            m_localPlayerBody = m_bodies[localID];
+
+            playState.RegisterUpdater(this);
+        }
+
+        public bool NeedsCorrection(ClientState remoteState)
+        {
+            bool err = false;
+            foreach(PlayerState state in remoteState.Players().Values)
+            {
+                err = (m_bodies[state.GUID].position - state.Position).sqrMagnitude > 0.01f;
+            }
+
+            return err;
+        }
+
+        public void SetStateAndStep(ref ClientState state, InputFrame input, float deltaTime)
+        {
+            foreach (PlayerState player in state.Players().Values)
+            {
+                player.Position.Set(m_bodies[player.GUID].position);
+            }
+            
+            common.logic.PlayerMovement.Execute(ref m_localPlayerBody, m_playerSettings.MovementSettings, input, deltaTime);
+        }
+
+        public void UpdateFromState(ClientState state)
+        {
+            foreach (PlayerState player in state.Players().Values)
+            {
+                m_bodies[player.GUID].position = player.Position;
+            }
+        }
+    }
+}
