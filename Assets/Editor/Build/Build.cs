ef677b759eb7da4541f119d38888457be484d6a4
ysma500 <ysmael.fortier@gmail.com>
2021-02-04 16:14:14 -0500

    Modify server and client build out in C# to stop conflicting folders

--- Assets/Editor/Build/Build.cs
+++ Assets/Editor/Build/Build.cs
@@ -10,7 +10,7 @@ public class Build
             "Assets/Scenes/ServerScene.unity",
             "Assets/Scenes/Prototypes/proto_move_server.unity"
         };
-        build.locationPathName = "serverBuild";
+        build.locationPathName = "ubivius_server";
         build.target = BuildTarget.StandaloneLinux64;
         build.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(build);
@@ -23,7 +23,7 @@ public class Build
             "Assets/Scenes/ClientScene.unity",
             "Assets/Scenes/Prototypes/proto_move_client.unity"
         };
-        build.locationPathName = "clientBuild";
+        build.locationPathName = "ubivius_client";
         build.target = BuildTarget.StandaloneWindows64;
         build.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(build);
