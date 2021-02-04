7b2a603fa4b0bd092656b7cdebb6caf7d5d645bd
ysma500 <ysmael.fortier@gmail.com>
2021-02-04 16:23:03 -0500

    Remove creation of build path in CI and let build func decide

--- Assets/Editor/Build/Build.cs
+++ Assets/Editor/Build/Build.cs
@@ -3,19 +3,6 @@ using System;
 
 public class Build
 {
-    static void ServerBuild()
-    {
-        BuildPlayerOptions build = new BuildPlayerOptions();
-        build.scenes = new[] {
-            "Assets/Scenes/ServerScene.unity",
-            "Assets/Scenes/Prototypes/proto_move_server.unity"
-        };
-        build.locationPathName = "ubivius_server";
-        build.target = BuildTarget.StandaloneLinux64;
-        build.options = BuildOptions.None;
-        BuildPipeline.BuildPlayer(build);
-    }
-
     static void ClientBuild()
     {
         BuildPlayerOptions build = new BuildPlayerOptions();
@@ -23,8 +10,21 @@ public class Build
             "Assets/Scenes/ClientScene.unity",
             "Assets/Scenes/Prototypes/proto_move_client.unity"
         };
-        build.locationPathName = "ubivius_client";
+        build.locationPathName = "clientBuild";
         build.target = BuildTarget.StandaloneWindows64;
+        build.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(build);
+    }
+
+    static void ServerBuild()
+    {
+        BuildPlayerOptions build = new BuildPlayerOptions();
+        build.scenes = new[] {
+            "Assets/Scenes/ServerScene.unity",
+            "Assets/Scenes/Prototypes/proto_move_server.unity"
+        };
+        build.locationPathName = "serverBuild";
+        build.target = BuildTarget.StandaloneLinux64;
         build.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(build);
     }
