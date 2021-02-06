d5013c69b4d8c02333fe1afe4d7e1651ed1847c4
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-02-06 03:10:25 -0500

    HealthsYSTEMpART1

---
+++ Assets/Scripts/HealthSystem/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gab {
+
+    public class HealthBar : MonoBehaviour 
+    {
+
+        private HealthSystem m_healthSystem;
+
+        public void Setup(HealthSystem m_healthSystem) {
+            this.m_healthSystem = m_healthSystem;
+
+            m_healthSystem.OnHealthChanged += m_healthSystem_Onm_healthChanged;
+            Updatem_healthBar();
+        }
+
+        private void m_healthSystem_Onm_healthChanged(object sender, System.EventArgs e) {
+            Updatem_healthBar();
+        }
+        private void Updatem_healthBar() {
+            transform.Find("Bar").localScale = new Vector3(m_healthSystem.GetHealthPercent(), 1);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gab {
+
+    public class HealthSystem {
+
+        public event EventHandler OnHealthChanged;
+        public event EventHandler OnDead;
+
+        private int m_healthMax;
+        private int m_health;
+
+        public HealthSystem(int m_healthMax) 
+        {
+            this.m_healthMax = m_healthMax;
+            m_healthMax = m_healthMax;
+        }
+        
+        public float GetHealthPercent() 
+        {
+            return (float)m_health / m_healthMax;
+        }
+
+        public void Damage(int amount) 
+        {
+            m_health -= amount;
+            if (m_health < 0) {
+                m_health = 0;
+            }
+            if (OnHealthChanged != null) OnHealthChanged(this, EventArgs.Empty);
+
+            if (m_health <= 0) {
+                Die();
+            }
+        }
+
+        public void Die() 
+        {
+            if (OnDead != null) OnDead(this, EventArgs.Empty);
+        }
+
+        public void Heal(int amount) 
+        {
+            m_health += amount;
+            if (m_health > m_healthMax) 
+            {
+                m_health = m_healthMax;
+            }
+            if (OnHealthChanged != null) OnHealthChanged(this, EventArgs.Empty);
+        }
+
+    }
+
+}
