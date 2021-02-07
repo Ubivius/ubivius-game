85f507113c15ba90874aac4107792b76183cf95e
ysma500 <ysmael.fortier@gmail.com>
2021-02-06 23:04:50 -0500

    Last push to get build outputs

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -10,7 +10,7 @@ public class Build
             "Assets/Scenes/ClientScene.unity",
             "Assets/Scenes/Prototypes/proto_move_client.unity"
         };
-        build.locationPathName = "/github/workspace/build/StandaloneWindows64/clientBuild.exe";
+        build.locationPathName = "/github/workspace/build/Client/ubivius-client.exe";
         build.target = BuildTarget.StandaloneWindows64;
         build.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(build);
@@ -23,7 +23,7 @@ public class Build
             "Assets/Scenes/ServerScene.unity",
             "Assets/Scenes/Prototypes/proto_move_server.unity"
         };
-        build.locationPathName = "/github/workspace/build/StandaloneLinux64/serverBuild";
+        build.locationPathName = "/github/workspace/build/Server/ubivius-server";
         build.target = BuildTarget.StandaloneLinux64;
         build.options = BuildOptions.None;
         BuildPipeline.BuildPlayer(build);
