641d6d9495d7371453ce0b4d3d544b99c7a53f2e
Jérôme Paquet <jerome.paquet@usherbrooke.ca>
2021-02-12 14:19:05 -0500

    Séparer en manager la gestion d'ajout de room, et ajout du void autour les pièce

---
+++ Assets/Scripts/Common/World/CorridorsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine;
+using ubv.common.world;
+
+namespace ubv.common.World
+{
+    class CorridorsManager
+    {
+        private Grid m_grid;
+
+        private ubv.common.world.LogicGrid m_masterLogicGrid;
+
+        public CorridorsManager(Grid grid, LogicGrid masterLogicGrid)
+        {
+            m_grid = grid;
+            m_masterLogicGrid = masterLogicGrid;
+        }
+
+        public LogicGrid GenerateCorridorsGrid()
+        {
+
+
+            return m_masterLogicGrid;
+        }
+    }
+}
--- Assets/Scripts/Common/World/WorldGenerator.cs
+++ Assets/Scripts/Common/World/WorldGenerator.cs
@@ -4,39 +4,7 @@ using UnityEngine;
 
 namespace ubv.common.world
 {
-    public struct BoxInfo
-    {
-        Vector2Int m_coord;
-        Vector3Int m_info;
-
-        public BoxInfo(int x, int y, int width, int height) : this()
-        {
-            m_coord.x = x;
-            m_coord.y = y;
-            m_info.x = width;
-            m_info.y = height;
-            m_info.z = width * height;
-        }
-        public Vector2Int GetOrigin()
-        {
-            return m_coord;
-        }
-        public int GetWidth() 
-        { 
-            return m_info.x; 
-        }
-        public int GetHeight()
-        {
-            return m_info.y;
-        }
-        public int GetArea()
-        {
-            return m_info.z;
-        }
-    }
-
     [RequireComponent(typeof(Grid))]
-
     public class WorldGenerator : MonoBehaviour
     {
         [SerializeField] private Vector2Int m_boundariesMap;
@@ -69,261 +37,46 @@ namespace ubv.common.world
 
         private Grid m_grid;
 
-        private const int c_mandatoryTry = 1000;
+        
 
         private ubv.common.world.LogicGrid m_masterLogicGrid;
+
+        private ubv.common.world.RoomManager m_roomManager;
+
+        private dataStruct.WorldGeneratorToRoomManager m_worldGeneratorToRoomManager;
 
         private void Awake()
         {
             m_grid = GetComponent<Grid>();
 
-            m_masterLogicGrid = new ubv.common.world.LogicGrid(m_boundariesMap.x, m_boundariesMap.y);
+            m_worldGeneratorToRoomManager = new dataStruct.WorldGeneratorToRoomManager(
+                m_boundariesMap,
+                m_randomRoomPoolSection0,
+                m_numberRandomRoomSection0,
+                m_numberofTrySection0,
+                m_mandatoryRoomPoolSection0,
+                m_randomRoomPoolTopLeft,
+                m_numberRandomRoomTopLeft,
+                m_numberofTryTopLeft,
+                m_mandatoryRoomPoolTopLeft,
+                m_randomRoomPoolTopRight,
+                m_numberRandomRoomTopRight,
+                m_numberofTryTopRight,
+                m_mandatoryRoomPoolTopRight,
+                m_randomRoomPoolBottomLeft,
+                m_numberRandomRoomBottomLeft,
+                m_numberofTryBottomLeft,
+                m_mandatoryRoomPoolBottomLeft,
+                m_randomRoomPoolBottomRight,
+                m_numberRandomRoomBottomRight,
+                m_numberofTryBottomRight,
+                m_mandatoryRoomPoolBottomRight,
+                m_grid);
 
-            Vector3 positionUnusedRoom = new Vector3(-250, -250, 0);
-            foreach (RoomInfo room in m_mandatoryRoomPoolSection0)
-            {
-                RoomInfo myRoom = Instantiate(room, positionUnusedRoom, Quaternion.identity, m_grid.transform);
-                AddRoomToSection0(myRoom, true);
-            }
-            for (int i = 0; i < m_numberRandomRoomSection0; i++)
-            {
-                RoomInfo myRoom = Instantiate(
-                    m_randomRoomPoolSection0[Random.Range(0, m_randomRoomPoolSection0.Count)],
-                    positionUnusedRoom, 
-                    Quaternion.identity, 
-                    m_grid.transform
-                    );
-                AddRoomToSection0(myRoom, false);
-            }
+            m_roomManager = new RoomManager(m_worldGeneratorToRoomManager);
+            m_masterLogicGrid = m_roomManager.GenerateRoomGrid();
+            
         }
-
-        private void AddRoom(RoomInfo room, Vector2Int roomOrigin)
-        {
-            room.transform.position = new Vector3(roomOrigin.x, roomOrigin.y, 0);
-            AddToMasterGrid(room, roomOrigin);
-        }
-
-        private void AddToMasterGrid(RoomInfo roomInfo, Vector2Int coord)
-        {
-            for (int x = 0; x < roomInfo.Width; x++)
-            {
-                for (int y = 0; y < roomInfo.Height; y++)
-                {
-                    m_masterLogicGrid.Grid[x + coord.x, y + coord.y] = roomInfo.LogicGrid.Grid[x, y];
-                }
-            }
-        }
-
-        private void AddRoomToSection0(RoomInfo room, bool isMandatory)
-        {
-            Vector2Int roomOrigin;
-            if (isMandatory)
-            {
-                roomOrigin = GetCoordInSection0(room);
-            }
-            else 
-            {
-                roomOrigin = GetCoordInSection0(room, m_numberofTrySection0);
-            }            
-            if (roomOrigin.x != -1)
-            {
-                AddRoom(room, roomOrigin);
-                return;
-            }
-        }
-
-
-        private void AddRoomToTopLeft(RoomInfo room, bool isMandatory)
-        {
-            Vector2Int roomOrigin;
-            if (isMandatory)
-            {
-                roomOrigin = GetCoordInTopLeft(room);
-            }
-            else
-            {
-                roomOrigin = GetCoordInTopLeft(room, m_numberofTryTopLeft);
-            }
-            if (roomOrigin.x != -1)
-            {
-                AddRoom(room, roomOrigin);
-                return;
-            }
-        }
-
-        private void AddRoomToTopRight(RoomInfo room, bool isMandatory)
-        {
-            Vector2Int roomOrigin;
-            if (isMandatory)
-            {
-                roomOrigin = GetCoordInTopRight(room);
-            }
-            else
-            {
-                roomOrigin = GetCoordInTopRight(room, m_numberofTryTopRight);
-            }
-            if (roomOrigin.x != -1)
-            {
-                AddRoom(room, roomOrigin);
-                return;
-            }
-        }
-
-        private void AddRoomToBottomLeft(RoomInfo room, bool isMandatory)
-        {
-            Vector2Int roomOrigin = GetCoordInBottomLeft(room);
-            if (roomOrigin.x != -1)
-            {
-                AddRoom(room, roomOrigin);
-                return;
-            }
-        }
-
-        private void AddRoomToBottomRight(RoomInfo room)
-        {
-            Vector2Int roomOrigin = GetCoordInBottomRight(room);
-            if (roomOrigin.x != -1)
-            {
-                AddRoom(room, roomOrigin);
-                return;
-            }
-        }
-
-        private Vector2Int TryGetCoord(RoomInfo roomInfo, BoxInfo bigBox, BoxInfo smallBox, int nbrTry)
-        {
-            Vector2Int coordTry;
-            for (int i = 0; i < nbrTry; i++)
-            {
-                coordTry = GetCoordInSection(bigBox, smallBox);
-                if (SpaceIsFree(roomInfo, coordTry))
-                {
-                    return coordTry;
-                }
-            }
-            return new Vector2Int(-1, -1);
-        }
-
-        private Vector2Int GetCoordInSection0(RoomInfo roomInfo, int nbrTry = c_mandatoryTry)
-        {
-            BoxInfo box = new BoxInfo(m_boundariesMap.x / 3, m_boundariesMap.y / 3, m_boundariesMap.x / 3, m_boundariesMap.y / 3);
-            Vector2Int coordTry;
-            for (int i = 0; i < nbrTry; i++)
-            {
-                coordTry = GetRandomBoxCoord(box);
-                if (SpaceIsFree(roomInfo, coordTry))
-                {
-                    return coordTry;
-                }
-            }
-            if(nbrTry == c_mandatoryTry)
-            {
-                Debug.LogError("MAP CREATION ALERT : Was not able to fit in SECTION0 mandatory room, look your sizing");
-            }            
-            return new Vector2Int(-1,-1);
-        }
-
-        private Vector2Int GetCoordInTopLeft(RoomInfo roomInfo, int nbrTry = c_mandatoryTry)
-        {
-            BoxInfo bigBox = new BoxInfo(0, m_boundariesMap.y / 2, m_boundariesMap.x / 3, m_boundariesMap.y / 2);
-            BoxInfo smallBox = new BoxInfo(m_boundariesMap.x / 3, m_boundariesMap.y / 3, m_boundariesMap.x / 6, m_boundariesMap.y / 3);
-            Vector2Int coord = TryGetCoord(roomInfo, bigBox, smallBox, nbrTry);
-            if (coord.x != -1)
-            {
-                return coord;
-            }
-            else if(nbrTry == c_mandatoryTry)
-            {
-                Debug.LogError("MAP CREATION ALERT : Was not able to fit in TOPLEFT mandatory room, look your sizing");
-            }
-            return new Vector2Int(-1, -1);
-        }
-
-        private Vector2Int GetCoordInTopRight(RoomInfo roomInfo, int nbrTry = c_mandatoryTry)
-        {
-            BoxInfo bigBox = new BoxInfo(m_boundariesMap.x * 2 / 3, m_boundariesMap.y / 2, m_boundariesMap.x / 3, m_boundariesMap.y / 2);
-            BoxInfo smallBox = new BoxInfo(m_boundariesMap.x / 2, m_boundariesMap.y * 2 / 3, m_boundariesMap.x / 6, m_boundariesMap.y / 3);
-            Vector2Int coord = TryGetCoord(roomInfo, bigBox, smallBox, nbrTry);
-            if (coord.x != -1)
-            {
-                return coord;
-            }
-            else if (nbrTry == c_mandatoryTry)
-            {
-                Debug.LogError("MAP CREATION ALERT : Was not able to fit in TOPRIGHT mandatory room, look your sizing");
-            }
-            return new Vector2Int(-1, -1);
-        }
-
-        private Vector2Int GetCoordInBottomLeft(RoomInfo roomInfo, int nbrTry = c_mandatoryTry)
-        {
-            BoxInfo bigBox = new BoxInfo(0, 0, m_boundariesMap.x / 3, m_boundariesMap.y / 2);
-            BoxInfo smallBox = new BoxInfo(m_boundariesMap.x / 3, 0, m_boundariesMap.x / 6, m_boundariesMap.y / 3);
-            Vector2Int coord = TryGetCoord(roomInfo, bigBox, smallBox, nbrTry);
-            if (coord.x != -1)
-            {
-                return coord;
-            }
-            else if (nbrTry == c_mandatoryTry)
-            {
-                Debug.LogError("MAP CREATION ALERT : Was not able to fit in BOTTOMLEFT mandatory room, look your sizing");
-            }
-            return new Vector2Int(-1, -1);
-        }
-
-        private Vector2Int GetCoordInBottomRight(RoomInfo roomInfo, int nbrTry = c_mandatoryTry)
-        {
-            BoxInfo bigBox = new BoxInfo(m_boundariesMap.x * 2 / 3, 0, m_boundariesMap.x / 3, m_boundariesMap.y / 2);
-            BoxInfo smallBox = new BoxInfo(m_boundariesMap.x / 2, 0, m_boundariesMap.x / 6, m_boundariesMap.y / 3);
-            Vector2Int coord = TryGetCoord(roomInfo, bigBox, smallBox, nbrTry);
-            if (coord.x != -1)
-            {
-                return coord;
-            }
-            else if (nbrTry == c_mandatoryTry)
-            {
-                Debug.LogError("MAP CREATION ALERT : Was not able to fit in BOTTOMRIGHT mandatory room, look your sizing");
-            }
-            return new Vector2Int(-1, -1);
-        }
-
-        private Vector2Int GetCoordInSection(BoxInfo bigBox, BoxInfo smallBox)
-        {
-
-            if (Random.Range(0,(bigBox.GetArea() + smallBox.GetArea())) < bigBox.GetArea())
-            {
-                return GetRandomBoxCoord(bigBox);
-            }
-            else
-            {
-                return GetRandomBoxCoord(smallBox);
-            }
-
-        }
-
-        private Vector2Int GetRandomBoxCoord(BoxInfo box)
-        {
-            return new Vector2Int(
-                    Random.Range(box.GetOrigin().x, box.GetOrigin().x + box.GetWidth()),
-                    Random.Range(box.GetOrigin().y, box.GetOrigin().y + box.GetHeight())
-                    );
-        }
-
-        // Il doit y avoir 6 uniter de libre autour de la room, 2 pour mur, 3 pour corridor et 1 pour mur
-        private bool SpaceIsFree(RoomInfo roomInfo, Vector2Int coord)
-        {
-            for (int x = coord.x - 6; x < coord.x + roomInfo.Width + 6; x++)
-            {
-                for (int y = coord.y - 6; y < coord.y + roomInfo.Height + 6; y++)
-                {
-                    if(m_masterLogicGrid.Grid[x, y] != null)
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
 
     }
 }
