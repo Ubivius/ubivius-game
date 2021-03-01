2674f61f29f4c13e0b7eb08312baf947dd7bcfd0
Charles <charles.murphy.042@gmail.com>
2021-03-01 14:47:52 -0500

    WIP on Unit tests

---
+++ Assets/Tests/HTTPClientTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class HTTPClientTest
+    {
+        [UnityTest]
+        public IEnumerator HTTPClientTest_Works()
+        {
+            yield return null;
+        }
+
+        public class HTTPClientMonoBehaviourTest : MonoBehaviour, IMonoBehaviourTest
+        {
+            bool IMonoBehaviourTest.IsTestFinished
+            {
+                get { return false; }
+            }
+        }
+
+        ubv.http.HTTPClient m_client;
+
+        private void Init()
+        {
+        }
+
+        [Test]
+        public void RunAllTests()
+        {
+            Init();
+            GetTest();
+            PostTest();
+            PutTest();
+            DeleteTest();
+        }
+
+        [Test]
+        public void GetTest()
+        {
+
+        }
+
+        [Test]
+        public void PostTest()
+        {
+
+        }
+
+        [Test]
+        public void PutTest()
+        {
+
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
