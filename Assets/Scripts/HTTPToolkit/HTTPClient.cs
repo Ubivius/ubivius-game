38c7a9c93d81a44038dcd1a8d93b155f8b4cc876
Charles <charles.murphy.042@gmail.com>
2021-03-01 11:47:53 -0500

    Added HTTPReceiver abstract class

--- Assets/Scripts/HTTPToolkit/HTTPClient.cs
+++ Assets/Scripts/HTTPToolkit/HTTPClient.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using UnityEngine;
 
-namespace ubv.common.http
+namespace ubv.http
 {
     /// <summary>
     /// HTTP client custom wrapper
@@ -20,55 +20,32 @@ namespace ubv.common.http
         {
             
         }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            Debug.Log(Get("products"));
-            PostJSON("products", "{\"name\":\"New Object added by Murphy\", \"price\":69.69, \"sku\":\"abc-abc-abcd\"}");
-            Debug.Log(Get("products"));
-            Delete("products/4");
-            Debug.Log(Get("products"));
-            Put("products", "{ \"id\":1 \"name\":\"Object put(modified) by Murphy\", \"price\":00.01, \"sku\":\"abc-abc-abcd\"}");
-            Debug.Log(Get("products"));
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
         
-        public void PostJSON(string requestUrl, string jsonString)
+        public HttpResponseMessage PostJSON(string requestUrl, string jsonString)
         {
             StringContent data = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
 
             HttpResponseMessage response = m_client.PostAsync(m_endPoint + "/" + requestUrl, data).Result;
-
-#if DEBUG_LOG
-            Debug.Log("POST response : " + response.Headers.ToString());
-#endif //DEBUG_LOG
+            
+            return response;
         }
 
-        public void Post(string requestUrl, object objToSerialize)
+        public HttpResponseMessage Post(string requestUrl, object objToSerialize)
         {
-            PostJSON(requestUrl, JsonUtility.ToJson(objToSerialize));
+            return PostJSON(requestUrl, JsonUtility.ToJson(objToSerialize));
         }
 
-        public void Put(string requestUrl, string jsonString)
+        public HttpResponseMessage Put(string requestUrl, string jsonString)
         {
             StringContent data = new StringContent(jsonString, System.Text.Encoding.UTF8, "application/json");
             
             HttpResponseMessage response = m_client.PutAsync(m_endPoint + "/" + requestUrl, data).Result;
-
-#if DEBUG_LOG
-            Debug.Log("PUT response : " + response.Headers.ToString());
-#endif //DEBUG_LOG
+            return response;
         }
 
-        public void Put(string requestUrl, object objToSerialize)
+        public HttpResponseMessage Put(string requestUrl, object objToSerialize)
         {
-            Put(requestUrl, JsonUtility.ToJson(objToSerialize));
+            return Put(requestUrl, JsonUtility.ToJson(objToSerialize));
         }
 
         public string Get(string requestUrl)
@@ -85,14 +62,11 @@ namespace ubv.common.http
             return responseContent;
         }
 
-        public void Delete(string requestUrl)
+        public HttpResponseMessage Delete(string requestUrl)
         {
             HttpResponseMessage result = m_client.DeleteAsync(m_endPoint + "/" + requestUrl).Result;
 
-            if (!result.IsSuccessStatusCode)
-            {
-                Debug.Log("DELETE to " + requestUrl + " failed");
-            }
+            return result;
         }
     }
 }
