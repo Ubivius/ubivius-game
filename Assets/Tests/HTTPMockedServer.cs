edd270913fa45c038f3a11fb99a60d51fbf65816
Charles <charles.murphy.042@gmail.com>
2021-03-02 15:11:59 -0500

    WIP on HTTPMockedServer

--- Assets/Tests/HTTPMockedServer.cs
+++ Assets/Tests/HTTPMockedServer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Net;
 using System.Threading.Tasks;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -11,12 +12,18 @@ namespace Tests
         private HttpListener m_httpListener;
         private readonly string m_prefix;
         private readonly string[] m_urls;
+        private List<string> m_productsJSONs;
 
         public HTTPMockedServer()
         {
             m_prefix = "http://localhost:9090";
             m_urls = new string[] {
                 "products"
+            };
+
+            m_productsJSONs = new List<string>()
+            {
+                "{\"id\":1, \"name\":\"newName\", \"price\":1.00, \"sku\":\"abc-abc-abcd\"}"
             };
 
             m_httpListener = new HttpListener();
@@ -39,9 +46,34 @@ namespace Tests
                 HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
                 
+                StringBuilder JSONBuilder = new StringBuilder();
+                foreach (string str in m_productsJSONs)
+                {
+                    JSONBuilder.Append(str);
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(JSONBuilder.ToString());
+
+                switch (request.HttpMethod)
+                {
+                    case "POST":
+                        break;
+                    case "PUT":
+                        break;
+                    case "GET":
+                        break;
+                    case "DELETE":
+                        break;
+                    default:
+                        break;
+                }
+
                 response.ContentType = "application/json";
                 response.ContentEncoding = Encoding.UTF8;
-                //response.ContentLength64 = data.LongLength;
+                response.ContentLength64 = data.LongLength;
+
+                await response.OutputStream.WriteAsync(data, 0, data.Length);
+                response.Close();
             }
         }
     }
