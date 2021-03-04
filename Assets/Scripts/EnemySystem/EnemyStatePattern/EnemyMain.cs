38f20ede10d599e3bf961f7981c7bcdb152b4014
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-03-04 01:55:20 -0500

    Bonjour la police... BONJOUR LA POLICE
    Gabarit général des enemis

---
+++ Assets/Scripts/EnemySystem/EnemyStatePattern/EnemyMain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.EnemySystem.EnemyStatePattern
+{
+    public class EnemyMain : MonoBehaviour
+    {
+        public EnemyPathFindingMovement EnemyPathFindingMovement { get; private set; }
+        public Rigidbody2D EnemyRigidbody2D { get; private set; }
+
+        //public HealthSystem HealthSystem { get; private set; }
+
+        private void Awake()
+        {
+            EnemyPathFindingMovement = GetComponent<EnemyPathFindingMovement>();
+            EnemyRigidbody2D = GetComponent<Rigidbody2D>();
+            //HealthSystem = new HealthSystem(100);
+        }
+
+        public Vector3 GetPosition()
+        {
+            return transform.position;
+        }
+    }
+}
