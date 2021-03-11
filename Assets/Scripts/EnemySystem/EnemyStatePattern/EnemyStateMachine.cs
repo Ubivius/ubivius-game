5a5e7f6d6881138b2241bd391d7067e97a4b6b5d
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-03-11 09:52:06 -0500

    cleaning State Machine stuff

--- Assets/Scripts/EnemySystem/EnemyStatePattern/EnemyStateMachine.cs
+++ Assets/Scripts/EnemySystem/EnemyStatePattern/EnemyStateMachine.cs
@@ -1,6 +1,4 @@
-﻿using Assets.EnemyAISystem.EnemyStatePattern;
-using Assets.Scripts.EnemySystem.EnemyStatePattern;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// In charge of updating the state of the enemy
@@ -10,16 +8,12 @@ namespace ubv.server.logic.ai
 {
     public class EnemyStateMachine : MonoBehaviour
     {
-        public EnemyMain EnemyMain { get; private set; }
-
         private EnemyState m_currentEnemyState;
-        
 
         // Use this for initialization
         void Start()
         {
-            EnemyMain = GetComponent<EnemyMain>();
-            m_currentEnemyState = new RoamingState(EnemyMain);
+            //m_currentEnemyState = new RoamingState()
         }
 
         // Update is called once per frame
