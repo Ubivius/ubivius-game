e638c9731c0fd89755d81948582a96e6de61dad1
Charles <charles.murphy.042@gmail.com>
2021-03-30 10:39:42 -0400

    Actually send ID message

--- Assets/Scripts/Client/ClientSyncStates/ClientSyncPlay.cs
+++ Assets/Scripts/Client/ClientSyncStates/ClientSyncPlay.cs
@@ -317,6 +317,7 @@ namespace ubv.client.logic
 #if DEBUG_LOG
             Debug.Log("Successful connection to server.");
 #endif // DEBUG_LOG
+            m_TCPClient.Send(new IdentificationMessage(m_playerID).GetBytes());
         }
 
         public void ReceivePacket(TCPToolkit.Packet packet)
