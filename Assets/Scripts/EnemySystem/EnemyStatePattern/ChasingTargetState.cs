e8e20b2f4004b2a90b4903b3923490611c0d0aa0
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-04-04 01:22:50 -0400

    ChaseStateLogic

---
+++ Assets/Scripts/EnemySystem/EnemyStatePattern/ChasingTargetState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using ubv.server.logic.ai;
+using UnityEngine;
+
+namespace Assets.Scripts.EnemySystem.EnemyStatePattern
+{
+    public class ChasingTargetState : EnemyState
+    {
+        private float m_attackRange = 30f;
+        private float m_stopChaseDistance = 80f;
+
+        // Update is called once per frame
+        public EnemyState Update()
+        {
+            pathfindingMovement.MoveTo(player.position/*Player.Instance.GetPosition*/);
+            //aimShootAnims.SetAimTarget(Player.Instance.GetPosition());
+
+            if (Vector3.SqrMagnitude(transform.position - player.position /*Player.Instance.GetPosition()*/) < m_attackRange)
+            {
+                // Target within attack range
+                //return new FightingState();
+            }
+
+            if (Vector3.SqrMagnitude(transform.position - player.position/*Player.Instance.GetPosition()*/) > m_stopChaseDistance * m_stopChaseDistance)
+            {
+                // Too far, stop chasing
+                //return new SearchingState();
+            }
+
+            return this;
+        }
+    }
+}
