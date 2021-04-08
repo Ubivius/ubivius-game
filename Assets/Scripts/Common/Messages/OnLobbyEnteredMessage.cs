1ede50d082a85a7b2ec2807a835a836a773c73e7
Charles <charles.murphy.042@gmail.com>
2021-04-07 20:41:09 -0400

    Correct tabulation

--- Assets/Scripts/Common/Messages/OnLobbyEnteredMessage.cs
+++ Assets/Scripts/Common/Messages/OnLobbyEnteredMessage.cs
@@ -4,16 +4,16 @@ using ubv.common.serialization;
 
 namespace ubv.common.data
 {
-        public class OnLobbyEnteredMessage : Serializable
+    public class OnLobbyEnteredMessage : Serializable
+    {
+        public OnLobbyEnteredMessage()
         {
-            public OnLobbyEnteredMessage()
-            {
-                InitSerializableMembers();
-            }
-                
-            protected override ID.BYTE_TYPE SerializationID()
-            {
-                return ID.BYTE_TYPE.LOBBY_ENTER_MESSAGE;
-            }
+            InitSerializableMembers();
         }
+                
+        protected override ID.BYTE_TYPE SerializationID()
+        {
+            return ID.BYTE_TYPE.LOBBY_ENTER_MESSAGE;
+        }
+    }
 }
