65e3c9e4a73a42324798fc9af51a72480d8c0ac3
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-04-14 01:34:04 -0400

    HealthBar first commit

---
+++ Assets/Scripts/HealthSystem/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private HealthSystem m_healthSystem;
+
+    public void Setup(HealthSystem healthSystem)
+    {
+        m_healthSystem = healthSystem;
+        m_healthSystem.OnHealthChanged.AddListener(HealthSystemOnHealthChanged);
+
+        UpdateHealthBar();
+    }
+
+    public void HealthSystemOnHealthChanged(int amount)
+    {
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        transform.Find("Bar").localScale = new Vector3(m_healthSystem.GetHealthPercent(), 1);
+    }
+}
--- Assets/Scripts/HealthSystem/HealthSystem.cs
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -56,28 +56,3 @@ public class HealthSystem : MonoBehaviour
         if (this.OnHealthChanged != null) this.OnHealthChanged.Invoke(amount);
     }
 }
-
-//Example of use
-/*
-public class HealthBar : MonoBehaviour
-{
-    private HealthSystem m_healthSystem;
-
-    public void Setup(HealthSystem healthSystem)
-    {
-        m_healthSystem = healthSystem;
-        m_healthSystem.OnHealthChanged.AddListener(HealthSystemOnHealthChanged);
-
-        UpdateHealthBar();
-    }
-
-    public void HealthSystemOnHealthChanged()
-    {
-        UpdateHealthBar();
-    }
-
-    private void UpdateHealthBar()
-    {
-        //transform.Find("Bar").localScale = new Vector3(m_healthSystem.GetHealthPercent(), 1);
-    }
-}*/
