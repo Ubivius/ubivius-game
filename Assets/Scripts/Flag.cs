203e0d0431af1415bead79974a5ac81704cba531
Charles <charles.murphy.042@gmail.com>
2021-04-19 11:20:54 -0400

    WIP on connection flow
    Simplified scene order

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace ubv.utils
+{
+    public class Flag
+    {
+        private bool m_value;
+
+        public Flag(bool defaultValue = false)
+        {
+            m_value = defaultValue;
+        }
+
+        public bool Read()
+        {
+            if (m_value)
+            {
+                m_value = false;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Peek()
+        {
+            return m_value;
+        }
+
+        public void Raise()
+        {
+            m_value = true;
+        }
+    }
+}
