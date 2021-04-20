fa2f5f27ad11660248ac6ea062727056fdc137b8
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-04-20 10:55:35 -0400

    Add todo comments

--- Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
+++ Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
@@ -7,16 +7,16 @@ using UnityEngine.Events;
 namespace ubv.client.logic
 {
     public class PlayerAnimator : MonoBehaviour
-    {   
-
-        [SerializeField] public GameObject m_player;
+    {  
         [SerializeField] public PlayerGameObjectUpdater m_playerGameObjectUpdater;
+
+        // To test the animation without the whole game and server setup
+        [SerializeField] public GameObject m_player;
         [SerializeField] public bool m_standAlone;
 
         private Rigidbody2D m_body;
         private Animator m_animator;
 
-        // Start is called before the first frame update
         void Start() {
             if (m_standAlone) {
                 m_animator = m_player.GetComponent<Animator>();
@@ -24,7 +24,6 @@ namespace ubv.client.logic
             }
         }
 
-        // Update is called once per frame
         void Update() {
             if (m_standAlone) {
                 m_animator.SetFloat("Speed", m_body.velocity.magnitude);
@@ -32,13 +31,15 @@ namespace ubv.client.logic
                 m_animator.SetFloat("Vertical", m_body.velocity.y);
             }
             else {
+                // TODO: Change the Bodies dict to the players dict
                 foreach (var body in m_playerGameObjectUpdater.Bodies) {
+                    // TODO: Update this line to get the animator from the player object and
+                    //       not from the body
                     m_animator = body.Value.gameObject.GetComponent<Animator>();
 
                     m_animator.SetFloat("Speed", body.Value.velocity.magnitude);
                     m_animator.SetFloat("Horizontal", body.Value.velocity.x);
                     m_animator.SetFloat("Vertical", body.Value.velocity.y);
-
                 }
             }
         }
