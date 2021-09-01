300ae8ebff147db82ef12041a89a24063131a6fa
Maxime St-Onge <maxime.stonge@usherbrooke.ca>
2021-09-01 14:03:32 -0400

    Add select and deseclect for tabBehavior

--- Assets/Scripts/UI/Client/LinkBehaviour.cs
+++ Assets/Scripts/UI/Client/LinkBehaviour.cs
@@ -7,25 +7,35 @@ using TMPro;
 namespace ubv.ui.client
 {
     [RequireComponent(typeof(Button))]
-    public class LinkBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class LinkBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
     {
-        TMP_Text m_selectable;
+        TMP_Text m_text;
         [SerializeField] private Color normalColor;
         [SerializeField] private Color hoverColor;
 
         private void Awake()
         {
-            m_selectable = this.GetComponentInChildren<TMP_Text>();
+            m_text = this.GetComponentInChildren<TMP_Text>();
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            m_selectable.color = hoverColor;
+            m_text.color = hoverColor;
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            m_text.color = hoverColor;
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            m_selectable.color = normalColor;
+            m_text.color = normalColor;
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            m_text.color = normalColor;
         }
     }
 }
