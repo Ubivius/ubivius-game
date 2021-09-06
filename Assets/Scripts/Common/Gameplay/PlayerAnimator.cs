c67a2e571acfce7fb73b58111680b1dd2521da77
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-09-06 15:40:01 -0400

    Create animation tree and add sprint action in prototype

--- Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
+++ Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
@@ -17,11 +17,21 @@ namespace ubv.client.logic
         private Rigidbody2D m_body;
         private Animator m_animator;
 
+        private Vector2 m_lastXVelocity;
+
+        private Vector2[] m_lastXVelocities;
+
         void Start() {
             if (m_standAlone) {
                 m_animator = m_player.GetComponent<Animator>();
                 m_body = m_player.GetComponent<Rigidbody2D>();
             }
+
+            m_lastXVelocities = new Vector2[4];
+        }
+
+        public void SetSprinting(bool isSprinting) {
+            m_animator.SetBool("IsSprinting", isSprinting);
         }
 
         void Update() {
@@ -29,17 +39,33 @@ namespace ubv.client.logic
                 m_animator.SetFloat("Speed", m_body.velocity.magnitude);
                 m_animator.SetFloat("Horizontal", m_body.velocity.x);
                 m_animator.SetFloat("Vertical", m_body.velocity.y);
+
+                if (m_body.velocity != m_lastXVelocity && m_body.velocity.magnitude > 0) {
+                    m_lastXVelocity = m_body.velocity;
+                    m_animator.SetFloat("Last Horizontal", m_lastXVelocity.x);
+                    m_animator.SetFloat("Last Vertical", m_lastXVelocity.y);
+                }
             }
             else {
                 // TODO: Change the Bodies dict to the players dict
+                int i = 0; 
                 foreach (var body in m_playerGameObjectUpdater.Bodies) {
                     // TODO: Update this line to get the animator from the player object and
                     //       not from the body
+
                     m_animator = body.Value.gameObject.GetComponent<Animator>();
 
                     m_animator.SetFloat("Speed", body.Value.velocity.magnitude);
                     m_animator.SetFloat("Horizontal", body.Value.velocity.x);
                     m_animator.SetFloat("Vertical", body.Value.velocity.y);
+
+                    if (body.Value.velocity != m_lastXVelocities[i] && body.Value.velocity.magnitude > 0) {
+                        m_lastXVelocities[i] = body.Value.velocity;
+                        m_animator.SetFloat("Last Horizontal", m_lastXVelocities[i].x);
+                        m_animator.SetFloat("Last Vertical", m_lastXVelocities[i].y);
+                    }
+
+                    i++;
                 }
             }
         }
--- Assets/Scripts/StandalonePlayerMovement.cs
+++ Assets/Scripts/StandalonePlayerMovement.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using ubv.client.logic;
 using ubv.common.gameplay;
 using ubv.common.logic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 namespace ubv.client
@@ -13,18 +15,28 @@ namespace ubv.client
         [SerializeField] PlayerController m_player;
 
         [SerializeField] private string m_scene = "proto_art";
+        [SerializeField] private PlayerAnimator m_playerAnimator;
 
         private PhysicsScene2D m_physics;
 
+        private bool m_isSprinting;
+
+        private UnityAction<bool> m_sprintAction;
 
 
         private void Awake()
         {
             m_physics = UnityEngine.SceneManagement.SceneManager.GetSceneByName(m_scene).GetPhysicsScene2D();
+            m_sprintAction += m_playerAnimator.SetSprinting;
         }
         
         private void FixedUpdate()
         {
+            if (InputController.CurrentFrame().Sprinting.Value != m_isSprinting) {
+                m_isSprinting = InputController.CurrentFrame().Sprinting.Value;
+                m_sprintAction.Invoke(m_isSprinting);
+            }
+
             PlayerMovement.Execute(ref m_body, m_player.GetStats(), InputController.CurrentFrame(), Time.fixedDeltaTime);
             m_physics.Simulate(Time.fixedDeltaTime);
         }
