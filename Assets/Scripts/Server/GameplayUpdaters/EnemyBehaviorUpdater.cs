9bdee37bc65ddda41d2734ea2fe61a5fe62de06c
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-09-29 13:20:13 -0400

    Petit commit dans mon cours d'histoire

--- Assets/Scripts/Server/GameplayUpdaters/EnemyBehaviorUpdater.cs
+++ Assets/Scripts/Server/GameplayUpdaters/EnemyBehaviorUpdater.cs
@@ -17,16 +17,27 @@ namespace ubv.server.logic
         public override void Setup()
         {
             //instantier un seul ennemy pur le moment
+            m_bodies = new Dictionary<int, Rigidbody2D>();
+            m_playerControllers = new Dictionary<int, common.gameplay.PlayerController>();
         }
 
         public override void InitClient(ClientState state)
         {
+            int id = state.EnemyGUID;
+            GameObject playerGameObject = GameObject.Instantiate(m_enemySettings.SimpleEnemy);
+            Rigidbody2D body = playerGameObject.GetComponent<Rigidbody2D>();
+            common.gameplay.PlayerController playerCtrl = playerGameObject.GetComponent<common.gameplay.PlayerController>();
+            body.position = m_gameMaster.GetPlayerSpawnPos();
+            body.name = "Server enemy " + id.ToString();
+            m_bodies.Add(id, body);
+
+            m_playerControllers.Add(state.PlayerGUID, playerCtrl);
 
         }
 
         public override void InitPlayer(PlayerState player)
         {
-
+            enemy.Position.Value = m_bodies[player.GUID.Value].position;
         }
 
         public override void FixedUpdateFromClient(ClientState client, InputFrame frame, float deltaTime)
