48bb3d5ba811dfa335275c525e9b6abdbe2bc4d2
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-10-06 16:19:55 -0400

    Remove comments

--- Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
+++ Assets/Scripts/Common/Gameplay/PlayerAnimator.cs
@@ -10,22 +10,17 @@ namespace ubv.client.logic
     {
         [SerializeField] private Rigidbody2D m_body;
         private Animator m_animator;
-        private bool m_isSprinting;
         private Vector2 m_lastXVelocity;
 
         void Start() {
             m_animator = GetComponent<Animator>();
-            //m_body = GetComponent<Rigidbody2D>();
         }
 
         public void SetSprinting(bool isSprinting) {
-            m_isSprinting = isSprinting;
             m_animator.SetBool("IsSprinting", isSprinting);
-            //Debug.Log("The player " + m_body.transform.parent.name + " sprinting is " + isSprinting + ".");
         }
 
         void Update() {
-            //Debug.Log("The player " + m_body.transform.name + " values are " + m_body.velocity.magnitude + ", " + m_body.velocity.x + ", " + m_body.velocity.y + "... Sprinting is: " + m_isSprinting);
             m_animator.SetFloat("Speed", m_body.velocity.magnitude);
             m_animator.SetFloat("Horizontal", m_body.velocity.x);
             m_animator.SetFloat("Vertical", m_body.velocity.y);
