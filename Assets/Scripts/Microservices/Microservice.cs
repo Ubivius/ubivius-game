f8ed53a50ca52832d7184fbc16660c70b10d3c65
Charles <charles.murphy.042@gmail.com>
2021-10-06 13:49:56 -0400

    WIP on microservice abstract class

---
+++ Assets/Scripts/Microservices/Microservice.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using ubv.http.client;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net;
+
+namespace ubv.microservices
+{
+    public abstract class Microservice : MonoBehaviour
+    {
+        public abstract class MicroserviceRequest
+        {
+            public abstract string GetURL();
+            public MicroserviceCallback Callback;
+        }
+        public abstract class MicroserviceCallback { }
+
+        public const int REQUEST_CHECK_TIME = 13;
+        protected readonly object m_requestLock = new object();
+
+        [SerializeField] protected bool m_mock;
+        [SerializeField] protected utils.Mocker m_mockData;
+
+        [SerializeField] protected HTTPClient m_HTTPClient;
+        [SerializeField] protected string m_serviceEndpoint;
+
+        protected bool m_readyForNextRequest;
+        protected Queue<MicroserviceRequest> m_requests;
+
+        private void Awake()
+        {
+            m_readyForNextRequest = true;
+            m_requests = new Queue<MicroserviceRequest>();
+        }
+
+        private void Update()
+        {
+            if (Time.frameCount % REQUEST_CHECK_TIME == 0)
+            {
+                lock (m_requestLock)
+                {
+                    if (m_readyForNextRequest)
+                    {
+                        if (m_requests.Count > 0)
+                        {
+                            MicroserviceRequest request = m_requests.Peek();
+                            SendRequest(request);
+                        }
+                    }
+                }
+            }
+        }
+        
+
+        protected void SendRequest(MicroserviceRequest request)
+        {
+            m_readyForNextRequest = false;
+            m_HTTPClient.SetEndpoint(m_serviceEndpoint);
+            m_HTTPClient.Get(request.GetURL(), OnResponse);
+        }
+
+        public void Request(MicroserviceRequest request)
+        {
+            if (m_mock)
+            {
+                Mock(request.Callback);
+                return;
+            }
+
+            lock (m_requestLock)
+            {
+                m_requests.Enqueue(request);
+
+                if (!m_readyForNextRequest)
+                {
+                    return;
+                }
+
+                SendRequest(request);
+            }
+        }
+
+        protected abstract void Mock(MicroserviceCallback callback);
+
+        protected abstract void OnResponse(string JSON, MicroserviceRequest originalRequest);
+
+        private void OnResponse(HttpResponseMessage message)
+        {
+            if (message.StatusCode == HttpStatusCode.OK)
+            {
+                string JSON = message.Content.ReadAsStringAsync().Result;
+                OnResponse(JSON, m_requests.Dequeue());
+            }
+            else
+            {
+#if DEBUG_LOG
+                Debug.Log("Request was not successful");
+#endif // DEBUG_LOG
+            }
+            m_readyForNextRequest = true;
+        }
+    }
+}
--- Assets/Scripts/Microservices/UserService.cs
+++ Assets/Scripts/Microservices/UserService.cs
@@ -7,25 +7,23 @@ using System.Collections.Generic;
 
 namespace ubv.microservices
 {
-    public class UserService : MonoBehaviour
+    public class UserService : Microservice
     {
-        public const int REQUEST_CHECK_DELAY = 13;
-        protected readonly object m_requestLock = new object();
-
-        [SerializeField] private bool m_mock;
-        [SerializeField] private utils.Mocker m_mockData;
-
-        [SerializeField] private HTTPClient m_HTTPClient;
-        [SerializeField] string m_userEndpoint;
         public delegate void OnGetInfo(UserInfo info);
-
-        private bool m_readyForNextRequest;
-        private Queue<UserInfoRequest> m_userRequests;
-
-        private class UserInfoRequest
+        
+        private class UserInfoRequest : Microservice.MicroserviceRequest
         {
             public string ID;
-            public OnGetInfo Callback;
+
+            public override string GetURL()
+            {
+                return "user/" + ID;
+            }
+        }
+
+        private class UserInfoCallback : MicroserviceCallback
+        {
+            public OnGetInfo OnGetInfo;
         }
         
         private struct JSONUserInfoResponse
@@ -51,92 +49,17 @@ namespace ubv.microservices
                 DateOfBirth = dateOfBirth;
             }
         }
-
-        private void Awake()
+        
+        protected override void OnResponse(string JSON, MicroserviceRequest originalRequest)
         {
-            m_readyForNextRequest = true;
-            m_userRequests = new Queue<UserInfoRequest>();
+            JSONUserInfoResponse userInfoResponse = JsonUtility.FromJson<JSONUserInfoResponse>(JSON);
+            UserInfo userInfo = new UserInfo(userInfoResponse.id, userInfoResponse.username, userInfoResponse.email, userInfoResponse.dateofbirth);
+            ((originalRequest as UserInfoRequest)?.Callback as UserInfoCallback)?.OnGetInfo.Invoke(userInfo);
         }
 
-        private void Update()
+        protected override void Mock(MicroserviceCallback callback)
         {
-            if (Time.frameCount % REQUEST_CHECK_DELAY == 0)
-            {
-                lock (m_requestLock)
-                {
-                    if (m_readyForNextRequest)
-                    {
-                        if (m_userRequests.Count > 0)
-                        {
-                            UserInfoRequest request = m_userRequests.Peek();
-                            Debug.Log("Requesting user info from " + request.ID + " from Update Loop");
-                            SendUserInfoRequest(request.ID);
-                        }
-                    }
-                }
-            }
-        }
-
-        public void SendUserInfoRequest(string id, OnGetInfo onGetInfo)
-        {
-            if (m_mock)
-            {
-#if DEBUG_LOG
-                Debug.Log("Mocking user. Auto logging in with random ID (or forced ID provided if any)");
-#endif // DEBUG_LOG
-                string _id = m_mockData.UserID.Length > 0 ? m_mockData.UserID : System.Guid.NewGuid().ToString();
-                string _user = m_mockData.UserName.Length > 0 ? m_mockData.UserName : "murphy-auto-username";
-                onGetInfo(new UserInfo(_id, _user, "murphy@gmail.com", "00-00-0001"));
-                return;
-            }
-#if DEBUG_LOG
-            Debug.Log("Requesting user info from " + id);
-#endif // DEBUG_LOG
-
-            lock (m_requestLock)
-            {
-                m_userRequests.Enqueue(new UserInfoRequest() { ID = id, Callback = onGetInfo });
-
-                Debug.Log("User requests count : " + m_userRequests.Count);
-
-                if (!m_readyForNextRequest)
-                {
-                    return;
-                }
-
-                SendUserInfoRequest(id);
-            }
-        }
-
-        private void SendUserInfoRequest(string id)
-        {
-            m_readyForNextRequest = false;
-            m_HTTPClient.SetEndpoint(m_userEndpoint);
-            m_HTTPClient.Get("users/" + id, OnUserResponse);
-        }
-
-        private void OnUserResponse(HttpResponseMessage message)
-        {
-            if (message.StatusCode == HttpStatusCode.OK)
-            {
-                // on s'attend à recevoir un token
-                // on add le token au HTTPClient
-
-                string JSON = message.Content.ReadAsStringAsync().Result;
-                JSONUserInfoResponse userInfoResponse = JsonUtility.FromJson<JSONUserInfoResponse>(JSON);
-                UserInfo userInfo = new UserInfo(userInfoResponse.id, userInfoResponse.username, userInfoResponse.email, userInfoResponse.dateofbirth);
-                Debug.Log("Received user info "  + userInfo­.ID + " (on OnUserResponse in service)");
-                Debug.Log("User request count in response : " + m_userRequests.Count);
-                Debug.Log("Calling callback on " + m_userRequests.Peek().ID);
-                m_userRequests.Dequeue().Callback.Invoke(userInfo);
-            }
-            else
-            {
-#if DEBUG_LOG
-                Debug.Log("User request was not successful");
-#endif // DEBUG_LOG
-            }
-            m_readyForNextRequest = true;
+            throw new System.NotImplementedException();
         }
     }
 }
