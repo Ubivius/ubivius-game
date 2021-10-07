246dfbe646ab0cd6723072786634e6bf55882efc
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-10-06 23:16:44 -0400

    petit commit avec changements de murphy

--- Assets/Scripts/Common/WorldState.cs
+++ Assets/Scripts/Common/WorldState.cs
@@ -23,12 +23,26 @@ namespace ubv.common
             }
         }
 
+        public class EnemyHashMap : HashMap<common.data.EnemyStateData>
+        {
+            public EnemyHashMap(Dictionary<int, common.data.EnemyStateData> enemies) : base(enemies)
+            { }
+
+            protected override ID.BYTE_TYPE SerializationID()
+            {
+                return ID.BYTE_TYPE.HASHMAP_INT_ENEMYSTATEDATA;
+            }
+        }
+
         private PlayerHashMap m_playerStates;
-        
+        private EnemyHashMap m_enemyStatesDatas;
+
         public WorldState() : base()
         {
             m_playerStates = new PlayerHashMap(new Dictionary<int, common.data.PlayerState>());
+            m_enemyStatesDatas = new EnemyHashMap(new Dictionary<int, common.data.EnemyStateData>());
             InitSerializableMembers(m_playerStates);
+            InitSerializableMembers(m_enemyStatesDatas);
         }
 
         public WorldState(System.Collections.Generic.List<common.data.PlayerState> playerStates) : base()
@@ -42,11 +56,27 @@ namespace ubv.common
             InitSerializableMembers(m_playerStates);
         }
 
+        public WorldState(System.Collections.Generic.List<common.data.EnemyStateData> enemyStatesDatas) : base()
+        {
+            Dictionary<int, common.data.EnemyStateData> dictStates = new Dictionary<int, data.EnemyStateData>();
+            foreach (data.EnemyStateData state in enemyStatesDatas)
+            {
+                dictStates.Add(state.GUID.Value, state);
+            }
+            m_enemyStatesDatas = new EnemyHashMap(dictStates);
+            InitSerializableMembers(m_playerStates);
+        }
+
         public WorldState(ref WorldState state) : base()
         {
             m_playerStates = new PlayerHashMap(new Dictionary<int, common.data.PlayerState>());
+            m_enemyStatesDatas = new EnemyHashMap(new Dictionary<int, common.data.EnemyStateData>());
+
             SetPlayers(state.m_playerStates.Value);
+            SetEnemies(state.m_enemyStatesDatas.Value);
+
             InitSerializableMembers(m_playerStates);
+            InitSerializableMembers(m_enemyStatesDatas);
         }
             
         protected override ID.BYTE_TYPE SerializationID()
@@ -59,6 +89,11 @@ namespace ubv.common
             m_playerStates.Value[playerState.GUID.Value] = playerState;
         }
 
+        public void AddEnemy(data.EnemyStateData enemyStateData)
+        {
+            m_enemyStatesDatas.Value[enemyStateData.GUID.Value] = enemyStateData;
+        }
+
         public void SetPlayers(Dictionary<int, common.data.PlayerState> playerStates)
         {
             m_playerStates.Value.Clear();
@@ -68,9 +103,23 @@ namespace ubv.common
             }
         }
 
+        public void SetEnemies(Dictionary<int, common.data.EnemyStateData> enemyStatesData)
+        {
+            m_enemyStatesDatas.Value.Clear();
+            foreach (common.data.EnemyStateData enemyStateData in enemyStatesData.Values)
+            {
+                m_enemyStatesDatas.Value[enemyStateData.GUID.Value] = new common.data.EnemyStateData();
+            }
+        }
+
         public Dictionary<int, common.data.PlayerState> Players()
         {
             return m_playerStates.Value;
+        }
+
+        public Dictionary<int, common.data.EnemyStateData> Enemies()
+        {
+            return m_enemyStatesDatas.Value;
         }
     }
 }
--- Assets/Scripts/Server/GameplayUpdaters/EnemyBehaviorUpdater.cs
+++ Assets/Scripts/Server/GameplayUpdaters/EnemyBehaviorUpdater.cs
@@ -26,37 +26,51 @@ namespace ubv.server.logic
             m_states = new Dictionary<int, EnemyStateMachine>();
         }
 
-        public override void InitClients(WorldState state)
+        public override void InitWorld(WorldState state)
         {
             m_pathfindingGridManager.OnPathFindingManagerGenerated += () => { OnPathFindingManagerGenerated(state); };
         }
 
         private void OnPathFindingManagerGenerated(WorldState state)
         {
-            int id = state.EnemyGUID;
-            GameObject enemyGameObject = GameObject.Instantiate(m_enemySettings.SimpleEnemy);
-            Rigidbody2D body = enemyGameObject.GetComponent<Rigidbody2D>();
-            EnemyPathFindingMovement enemyPathFindingMovement = enemyGameObject.GetComponent<EnemyPathFindingMovement>();
-            EnemyStateMachine enemyStateMachine = enemyGameObject.GetComponent<EnemyStateMachine>();
-            enemyPathFindingMovement.SetManager(m_pathfindingGridManager);
-            body.position = enemyPathFindingMovement.GetPosition();
-            body.name = "Server enemy " + id.ToString() + enemyStateMachine.CurrentEnemyState;
+            foreach (int id in state.Players().Keys)
+            {
+                GameObject enemyGameObject = GameObject.Instantiate(m_enemySettings.SimpleEnemy);
+                Rigidbody2D body = enemyGameObject.GetComponent<Rigidbody2D>();
+                EnemyPathFindingMovement enemyPathFindingMovement = enemyGameObject.GetComponent<EnemyPathFindingMovement>();
+                EnemyStateMachine enemyStateMachine = enemyGameObject.GetComponent<EnemyStateMachine>();
+                enemyPathFindingMovement.SetManager(m_pathfindingGridManager);
 
-            m_states.Add(id, enemyStateMachine);
-            m_bodies.Add(id, body);
+                body.position = enemyPathFindingMovement.GetPosition();
+                body.name = "Server enemy " + id.ToString() + enemyStateMachine.CurrentEnemyState;
+
+                m_states.Add(id, enemyStateMachine);
+                m_bodies.Add(id, body);
+
+                EnemyStateData enemyStateData = new EnemyStateData(id);
+
+                enemyStateData.Position.Value = m_bodies[id].position;
+
+                m_enemyStatesData.Add(id, enemyStateData);
+            }
+
+            foreach (EnemyStateData enemy in m_enemyStatesData.Values)
+            {
+                state.AddEnemy(enemy);
+            }
         }
 
-        public override void FixedUpdateFromClient(WorldState client, InputFrame frame, float deltaTime)
+        public override void FixedUpdateFromClient(WorldState client, Dictionary<int, InputFrame> frames, float deltaTime)
         {
         }
 
-        public override void UpdateClient(ref WorldState client)
+        public override void UpdateWorld(WorldState client)
         {
             foreach (int id in client.Enemies().Keys)
             {
                 Rigidbody2D body = m_bodies[id];
                 EnemyStateData enemy = m_enemyStatesData[id];
-                EnemyStateMachine enemyStateMachine = m_states[client.EnemyGUID];
+                EnemyStateMachine enemyStateMachine = m_states[id];
                 enemy.Position.Value = body.position;
                 enemy.Rotation.Value = body.rotation;
                 enemy.EnemyState = enemyStateMachine.CurrentEnemyState;
