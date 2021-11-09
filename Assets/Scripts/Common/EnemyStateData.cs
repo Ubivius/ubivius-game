9545fdef6242a2bf012781210a7a50c06c8b93fd
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-11-09 14:51:38 -0500

    Update EnemyStateData.cs

--- Assets/Scripts/Common/EnemyStateData.cs
+++ Assets/Scripts/Common/EnemyStateData.cs
@@ -16,7 +16,6 @@ namespace ubv
             /// </summary>
             ///
             
-            // add enemystateinfo comme cellinfo
             public class EnemyStateData : serialization.Serializable
             {
                 // send this over network
@@ -28,17 +27,6 @@ namespace ubv
 
                 public EnemyStateInfo EnemyStateInfo;
                 public EnemyState EnemyState;
-
-                /*private serialization.types.Byte m_enemyState;
-                private serialization.types.Int32 m_enemyStateID;
-                private serialization.types.ByteArray m_enemyStateBytes;*/
-                /*public enum EnemyStateType
-                {
-                    ROAMING,
-                    SEARCHING,
-                    CHASING,
-                    FIGHTING
-                }*/
 
                 // TODO : not send this via network because not likely to be changed
                 public serialization.types.Int32 GUID;
@@ -72,30 +60,6 @@ namespace ubv
 
                     InitSerializableMembers(Position, Rotation, GUID, GoalPosition);
                 }
-
-                /*public EnemyState EnemyStateFromBytes()
-                {
-                    EnemyState enemyState = null;
-                    switch ((EnemyStateType)m_enemyState.Value)
-                    {
-                        case EnemyStateType.ROAMING:
-                            enemyState = CreateFromBytes<RoamingState>(m_enemyStateBytes.Value.ArraySegment());
-                            break;
-                        case EnemyStateType.SEARCHING:
-                            enemyState = CreateFromBytes<SearchingState>(m_enemyStateBytes.Value.ArraySegment());
-                            break;
-                        case EnemyStateType.CHASING:
-                            enemyState = CreateFromBytes<ChasingState>(m_enemyStateBytes.Value.ArraySegment());
-                            break;
-                        case EnemyStateType.FIGHTING:
-                            enemyState = CreateFromBytes<FightingState>(m_enemyStateBytes.Value.ArraySegment());
-                            break;
-                        default:
-                            break;
-                    }
-
-                    return enemyState;
-                }*/
 
                 protected override ID.BYTE_TYPE SerializationID()
                 {
