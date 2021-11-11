8db3c4aea0060fd05461c5ca711058df00af91ec
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-11-11 15:59:46 -0500

    WIP

---
+++ Assets/Scripts/UI/Client/Achievement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static ubv.microservices.CharacterDataService;
+
+namespace ubv.ui.client {
+    public class Achievement: MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI m_description;
+        [SerializeField] private TextMeshProUGUI m_name;
+        [SerializeField] private Image m_icon;
+        [SerializeField] private Transform m_descrition;
+
+        private bool m_isActive;
+
+        private bool m_isVisible = false;
+
+        public void ToggleDescrition() {
+            m_isActive != m_isActive;
+        }
+
+        public bool IsVisible()
+        {
+            return m_isVisible;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Client/ClientAchievementsUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using static ubv.microservices.CharacterDataService;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static ubv.client.logic.ClientAchievementsState;
+using UnityEngine.EventSystems;
+
+namespace ubv.ui.client
+{
+    public class ClientAchievementsUI : TabBehaviour
+    {
+        [SerializeField] private ubv.client.logic.ClientAchievementsState m_initState;
+        private EventSystem system;
+
+        private void Awake()
+        {
+            system = EventSystem.current;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        public void Back()
+        {
+            m_initState.Back();
+        }
+
+    }
+}
