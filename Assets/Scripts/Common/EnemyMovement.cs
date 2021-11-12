d09f643bc0b8b07dd082b9c771ddb29d52555ce6
Daftdoritos <36829260+Daftdoritos@users.noreply.github.com>
2021-11-11 21:57:42 -0500

    retest pr naming

--- Assets/Scripts/Common/EnemyMovement.cs
+++ Assets/Scripts/Common/EnemyMovement.cs
@@ -13,7 +13,7 @@ namespace ubv
             public class EnemyMovement
             {
                 static public void Execute(Rigidbody2D rigidbody, Vector2 targetPosition, float speed)
-                {//
+                {
                     rigidbody.velocity = (targetPosition - rigidbody.position).normalized * speed;
                 }
             }
