47676daf32afcae845303a7722e50a20f180b474
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-11-12 13:31:19 -0500

    Some dynamic achievement with dropdown for description

--- Assets/Scripts/UI/Client/Achievement.cs
+++ Assets/Scripts/UI/Client/Achievement.cs
@@ -1,31 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static ubv.microservices.CharacterDataService;
 
 namespace ubv.ui.client {
-    public class Achievement: MonoBehaviour {
-        [SerializeField] private TextMeshProUGUI m_description;
-        [SerializeField] private TextMeshProUGUI m_name;
+    public class Achievement: MonoBehaviour
+    {
+        [SerializeField] private Color m_baseColor;
+        [SerializeField] private Color m_activeColor;
+        [SerializeField] private Color m_dropdownColor;
+        [SerializeField] private Color m_lockedColor;
+
+        [SerializeField] private GameObject m_body;
+
+        [SerializeField] private GameObject m_header;
+
         [SerializeField] private Image m_icon;
-        [SerializeField] private Transform m_descrition;
+        [SerializeField] private Button m_button;
+        [SerializeField] private TextMeshProUGUI m_name;
+        [SerializeField] private TextMeshProUGUI m_date;
+        [SerializeField] private TextMeshProUGUI m_description;
+
+        [SerializeField] private GameObject m_dropDown;
 
         private bool m_isActive;
+        
 
-        private bool m_isVisible = false;
+        void Start() {
+            m_header.GetComponent<Image>().color = m_baseColor;
+            m_dropDown.GetComponent<Image>().color = m_dropdownColor;
 
-        public void ToggleDescrition() {
-            m_isActive != m_isActive;
+            m_button = m_header.GetComponent<Button>();
+            //m_button.interactable = false;
         }
 
-        public bool IsVisible()
-        {
-            return m_isVisible;
+        public void ToggleDropdown() {
+            m_isActive = !m_isActive;
+
+            if (m_isActive)
+            {
+                m_header.GetComponent<Image>().color = m_activeColor;
+                m_dropDown.SetActive(true);
+            }
+            else
+            {
+                m_header.GetComponent<Image>().color = m_baseColor;
+                m_dropDown.SetActive(false);
+            }
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(m_body.transform.parent.gameObject.GetComponent<RectTransform>());
+        }
+
+        public void Unlock() {
+            m_button.interactable = true;
+            m_icon.color = Color.green;
+        }
+
+        public void InitializeAchievement(string a_name, string a_description, string a_date, Image a_newIcon) {
+            //m_icon = a_newIcon;
+            m_icon.color = Color.red;
+            m_name.text = a_name;
+            m_date.text = a_date;
+            m_description.text = a_description;
         }
     }
 }
--- Assets/Scripts/UI/Client/ClientAchievementsUI.cs
+++ Assets/Scripts/UI/Client/ClientAchievementsUI.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security;
 using TMPro;
-using static ubv.microservices.CharacterDataService;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static ubv.client.logic.ClientAchievementsState;
 using UnityEngine.EventSystems;
 
 namespace ubv.ui.client
 {
     public class ClientAchievementsUI : TabBehaviour
     {
-        [SerializeField] private ubv.client.logic.ClientAchievementsState m_initState;
-        private EventSystem system;
+        [SerializeField] private GameObject m_achievementPrefab;
+        [SerializeField] private GameObject m_content;
+
+        private List<Achievement> m_achievements;
+
 
         private void Awake()
         {
-            system = EventSystem.current;
+            m_achievements = new List<Achievement>();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            GameObject test_achie = Instantiate(m_achievementPrefab, m_content.transform);
+            test_achie.GetComponent<Achievement>().InitializeAchievement("Get Good", "HAHAHAHAHAHA HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA HAHAHAHAHAHAHAHAHAHAHAHA You're slowly making it there!", "01/23/2021", null);
+            test_achie.GetComponent<Achievement>().Unlock();
+
+            GameObject test_achie2 = Instantiate(m_achievementPrefab, m_content.transform);
+            test_achie2.GetComponent<Achievement>().InitializeAchievement("You're so fking bad", "Read the title.", "02/23/1999", null);
+            test_achie2.GetComponent<Achievement>().Unlock();
+
+            GameObject test_achie3 = Instantiate(m_achievementPrefab, m_content.transform);
+            test_achie3.GetComponent<Achievement>().InitializeAchievement("Hehe", "test", "01/23/2069", null);
         }
 
         protected override void Update()
@@ -27,7 +45,7 @@ namespace ubv.ui.client
 
         public void Back()
         {
-            m_initState.Back();
+            //Go to game main menu scene
         }
 
     }
