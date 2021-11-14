fc8dd7639ccbcce40725cc14d99f7cba60f4b878
Raphael Lebrasseur <raf_01_leb@hotmail.com>
2021-11-14 00:43:38 -0500

    wip

---
+++ Assets/Scripts/UI/Client/PopupUI.cs
@@ -0,0 +1,15 @@
+﻿
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ubv.ui.client {
+    public class Popup : MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI m_title;
+        [SerializeField] private TextMeshProUGUI m_message;
+        [SerializeField] private Transform m_placeholder;
+
+        
+
+    }
+}
