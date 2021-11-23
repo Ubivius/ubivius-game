e466d76d1b138ba256da2a8b184afa5fde2c04ea
gstpierre <stpg2308@usherbrooke.ca>
2021-11-22 22:15:12 -0500

    WIP

---
+++ Assets/Scripts/Client/ClientSyncStates/ClientAchievementState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ubv.client.logic
+{
+    /// <summary>
+    /// Represents the client state in the achievements menu
+    /// </summary>
+    public class ClientAchievementState : ClientSyncState
+    {
+        [SerializeField] ubv.microservices.AchievementService m_achievementService;
+        [SerializeField] ubv.ui.client.ClientAchievementUI m_clientAchievementUI;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            m_canBack = true;
+        } 
+
+        protected override void StateLoad()
+        {
+            GetAllAchievements();
+        }
+
+        public override void StateUpdate()
+        {
+        }
+
+        public void GetAllAchievements()
+        {
+            m_achievementService.Request(new ubv.microservices.GetAllAchievementsRequest(m_clientAchievementUI.CreateAchievement));
+        }
+
+        protected override void StateUnload()
+        {
+        }
+
+        protected override void StatePause()
+        {
+        }
+
+        protected override void StateResume()
+        {
+        }
+
+        public void GoBackToPreviousState()
+        {
+            ClientStateManager.Instance.PopState();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Client/ClientAchievementUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace ubv.ui.client
+{
+    public class ClientAchievementUI : TabBehaviour
+    {
+        [SerializeField] private ubv.client.logic.ClientAchievementState m_achievementState;
+        [SerializeField] private GameObject m_achievementPrefab;
+        [SerializeField] private GameObject m_content;
+
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        public void Back()
+        {
+            //Go to game main menu scene
+        }
+
+        public void CreateAchievement(ubv.microservices.AchievementService.Achievement[] achievements)
+        {
+            foreach (ubv.microservices.AchievementService.Achievement achievement in achievements)
+            {
+                GameObject achievementInstance = Instantiate(m_achievementPrefab, m_content.transform);
+                achievementInstance.GetComponent<Achievement>().InitializeAchievement(achievement.Name, achievement.Description, "image");
+                achievementInstance.GetComponent<Achievement>().Unlock();
+            }
+        }
+    }
+}
